c9b60fefbbbfc492d4bcc0f97d91718858d05b3a
Mateus.nasciment <106212780+Mateusnasciment@users.noreply.github.com>
2023-11-26 13:59:53 -0300

    config: all

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #! /usr/bin/env bash
 
-# replace all module references
+# Substitua todas as referências ao módulo antigo
 CURR_MODULE='github.com/go-web-templates/api'
-NEW_MODULE=$1
+NEW_MODULE='github.com/mateusnasciment/APIGOLANG'
 
-echo "Replacing the module name"
+echo "Substituindo o nome do módulo"
 find . -type f -name "*.go" -print0 | \
   xargs -0 sed -i '' -e "s#${CURR_MODULE}#${NEW_MODULE}#g" \
   2>/dev/null
 
-echo "Deleting old go module files"
+echo "Excluindo os antigos arquivos de módulo Go"
 rm -rf go.mod go.sum
 
-echo "Creating a new go module"
+echo "Criando um novo módulo Go"
 go mod init ${NEW_MODULE}
 go mod tidy
